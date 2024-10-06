4a1bb5b074ccff6531a77d814fcf1bc81a7916fd
chudistar01 <christianigwebuike31@gmail.com>
2024-10-06 20:30:34 +0000

    cache

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
-
-'''Task 1
-'''
+"""Task 1
+"""
 
 
 from collections import OrderedDict
@@ -9,22 +8,22 @@ from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    '''Subclass thhhat inherits from base class
-    '''
+    """Subclass thhhat inherits from base class
+    """
     def __init__(self):
+        """instance method"""
         super().__init__()
         self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        '''Assign value to dictionary
-        '''
+        """Assign value to dictionary
+        """
 
         if key is None or item is None:
             return
         if key in self.cache_data:
             # Update the existing key
             self.cache_data[key] = item
-
 
         if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
             first_key, first_value = self.cache_data.popitem(last=False)
@@ -33,15 +32,15 @@ class FIFOCache(BaseCaching):
         self.cache_data[key] = item
 
     def get(self, key):
-        '''Returns thhe value in thhe dictionary
-        '''
+        """Returns thhe value in thhe dictionary
+        """
         if key is None or key not in self.cache_data:
             return None
         return self.cache_data[key]
 
     def print_cache(self):
-        '''prints cache'''
+        """prints cache
+        """
         print("Current cache:")
         for key, value in self.cache_data.items():
             print(f"{key}: {value}")
-        
